df0df0aebf6470519f8036d47c6ebb03cd250909
Kevin Wong <kevinwong@pursuit.org>
2020-01-11 16:49:31 -0500

    set up backend and html

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
+const port = 3005;
 const app = express();
+app.use(cors());
+
+let animalsArr = ["lion", "zebra", "tiger", "elephant", "shark"];
+
+const isAnimal = (req, res, next) => {
+    console.log(req.params);
+    if(animalsArr.includes(req.params.name)){
+        res.json({
+            status: "Success",
+            message: true,
+        })
+    } else {
+        res.json({
+            status: "Failed",
+            message: false,
+        })
+    }
+}
+
+app.get("/animal", (req, res, next) => {
+    res.json({animalsArr});
+})
+
+app.get("/animal/:name", isAnimal, (req, res) => {
+    res.json(animalsArr);
+})
+
 
 app.listen(port, () => console.log("Listening on port: ", port));
