82e9248fdeddb96cf579eb461671f240e88a1a24
Kevin Wong <kevinwong@pursuit.org>
2020-01-11 20:23:58 -0500

    complete front end for problem2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let container1 = document.querySelector("container1")
+    let container1 = document.querySelector("#container1")
     let input = document.querySelector("#input")
     let resultBtn = document.querySelector("#resultBtn")
 
@@ -16,8 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 let h4 = document.createElement("h4");
                 h3.innerText = `Checking if animal is in our database...`;
                 setTimeout(() => {
-                    h4.innerText = `Status: ${res.data.status}` + '\n' + `Animal is in database: ${res.data.message}`;
-                }, 2000);
+                    h4.innerText = `Status: ${res.data.status}` + '\n' + `Animal available: ${res.data.message}`;
+                }, 1500);
                 container1.appendChild(h3);
                 container1.appendChild(h4);
             })
@@ -29,7 +29,23 @@ document.addEventListener("DOMContentLoaded", () => {
     let ceil = document.querySelector("#ceil");
     let randomizeBtn = document.querySelector("#randomizeBtn");
     randomizeBtn.addEventListener("click", () => {
-        
+        if(floor.value === "" || ceil.value === ""){
+            let h1 = document.createElement("h1");
+            h1.innerText = "Please enter a valid range!"
+            container2.appendChild(h1);
+        } else {
+            axios.get(`http://localhost:3002/random?floor=${floor.value}&ceil=${ceil.value}`).then(res => {
+                debugger
+                let h3 = document.createElement("h3")
+                let h4 = document.createElement("h4");
+                h3.innerText = `Generating a random number from your range...`;
+                setTimeout(() => {
+                    h4.innerText = `Status: ${res.data.status}` + '\n' + `Range: ${res.data.range}` + '\n' + `Your random number is: ${res.data.randPick}`;
+                }, 1500);
+                container2.appendChild(h3);
+                container2.appendChild(h4);
+            })
+        }
     })
     
     
