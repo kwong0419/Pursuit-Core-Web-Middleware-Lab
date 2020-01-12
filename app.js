a40196b9cc5888bd47e9f3e8d0519986fc313690
Kevin Wong <kevinwong@pursuit.org>
2020-01-11 20:05:12 -0500

    complete backend for problem2

--- app.js
+++ app.js
@@ -21,21 +21,37 @@ const isAnimal = (req, res, next) => {
     }
 }
 
-
 app.get("/animal", (req, res, next) => {
     res.json({animalsArr});
 })
-
 
 app.get("/animal/:name", isAnimal, (req, res) => {
     res.json(animalsArr);
 })
 
-app.get("/random", (req, res, next) => {
+
+const generateSpread = (req, res, next) => {
+    console.log(req.params);
+    let range = [];
+    for(let i = Number(req.query.floor); i < Number(req.query.ceil); i++){
+        range.push(i);
+    }
+    res.status = "Success";
+    res.randomPick= range[Math.floor(Math.random() * range.length)];
+    next();
+}
+
+app.get("/random", generateSpread, (req, res) => {
     res.json({
-        
+        status: res.status,
+        range: [Number(req.query.floor), Number(req.query.ceil)],
+        randPick: res.randomPick,
     });
 })
+
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let container = document.querySelector("#container")
+    let container1 = document.querySelector("container1")
     let input = document.querySelector("#input")
     let resultBtn = document.querySelector("#resultBtn")
 
 
     resultBtn.addEventListener("click", () => {
-        container.innerHTML = "";
+        container1.innerHTML = "";
         if(input.value === ""){
             let h1 = document.createElement("h1");
             h1.innerText = "Please enter an animal";
-            container.appendChild(h1);
+            container1.appendChild(h1);
         } else {
-            axios.get(`http://localhost:3005/animal/${input.value}`).then(res => {
+            axios.get(`http://localhost:3002/animal/${input.value}`).then(res => {
                 let h3 = document.createElement("h3")
                 let h4 = document.createElement("h4");
                 h3.innerText = `Checking if animal is in our database...`;
                 setTimeout(() => {
                     h4.innerText = `Status: ${res.data.status}` + '\n' + `Animal is in database: ${res.data.message}`;
                 }, 2000);
-                container.appendChild(h3);
-                container.appendChild(h4);
+                container1.appendChild(h3);
+                container1.appendChild(h4);
             })
         }
     })
 
-    let floor = document.querySelector("#floor")
-    let ceil = document.querySelector("#ceil")
+    let container2 = document.querySelector("#container2");
+    let floor = document.querySelector("#floor");
+    let ceil = document.querySelector("#ceil");
+    let randomizeBtn = document.querySelector("#randomizeBtn");
+    randomizeBtn.addEventListener("click", () => {
+        
+    })
     
     
     
